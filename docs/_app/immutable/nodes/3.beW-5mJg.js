a20077b8b5b2f5ade15168d181994b6a7645a21c
Timothy Kempf <tim@kempf.dev>
2024-09-08 11:05:34 -0700

    add static site in correct directory for firebase hosting

--- docs/_app/immutable/nodes/3.beW-5mJg.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/3.CkL0OewH.js";export{e as component,n as universal};
--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapterStatic({
-			pages: 'docs',
-			assets: 'docs',
+			pages: 'public',
+			assets: 'public',
 			fallback: 'index.html',
 			precompress: true
 		}),
