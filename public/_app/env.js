e0972359f9e9358bc1f498c040f46733d9dd32dc
Timothy Kempf <tim@kempf.dev>
2024-09-09 08:04:03 -0700

    remove compiled files

--- public/_app/env.js
+++
@@ -1,1 +0,0 @@
-export const env={"PUBLIC_FIREBASE_API_KEY":"AIzaSyDriffF4c33V459hiSyEkFmUZAQaportrs","PUBLIC_FIREBASE_DATABASE_URL":"not-firebase-58b83.firebaseapp.com","PUBLIC_FIREBASE_STORAGE_BUCKET":"not-firebase-58b83.appspot.com","PUBLIC_FIREBASE_PROJECT_ID":"not-firebase-58b83"}
