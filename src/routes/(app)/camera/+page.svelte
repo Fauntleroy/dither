bc8ae6f8549221e44c97cf2e1259eec0dbf7cb44
Timothy Kempf <tim@kempf.dev>
2024-09-15 16:36:56 -0700

    actually control error page

---
+++ src/routes/(app)/camera/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import StylizedWebcamFeed from '$/components/stylized-webcam-feed.svelte';
+
+	function handleMount() {
+		console.log('mounted', arguments);
+	}
+</script>
+
+<div><StylizedWebcamFeed onMount={handleMount} /></div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const { status, error } = $page;
+
+	const dev = process.env.NODE_ENV === 'development';
+</script>
+
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
+
+<div class="errorPage">
+	<div class="emoji">☹</div>
+
+	<h1>{status}</h1>
+
+	<p>{error?.message}</p>
+
+	{#if dev && error?.stack}
+		<pre>{error?.stack}</pre>
+	{/if}
+</div>
+
+<style>
+	.errorPage {
+		min-height: 100vh;
+		min-width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
