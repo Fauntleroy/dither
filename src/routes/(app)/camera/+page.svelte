96bd3b00ef6efff5b03cc3212ba4af2c0f87d4a4
Timothy Kempf <tim@kempf.dev>
2024-09-16 08:45:28 -0700

    add new camera page

--- src/routes/(app)/camera/+page.svelte
+++ src/routes/(app)/camera/+page.svelte
@@ -1,9 +1,12 @@
 <script>
-	import StylizedWebcamFeed from '$/components/stylized-webcam-feed.svelte';
+	import CameraSelector from '$/components/camera-selector.svelte';
 
 	function handleMount() {
 		console.log('mounted', arguments);
 	}
 </script>
 
-<div><StylizedWebcamFeed onMount={handleMount} /></div>
+<!-- <div><StylizedWebcamFeed onMount={handleMount} /></div> -->
+<div>
+	<CameraSelector />
+</div>
