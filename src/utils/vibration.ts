4ca77632163ffd55967192c1f43209fb2acce835
Timothy Kempf <tim@kempf.dev>
2024-09-22 08:13:21 -0700

    clean up vibration method

--- src/utils/vibration.ts
+++ src/utils/vibration.ts
@@ -1,11 +1,8 @@
 export function hapticBuzz() {
 	if ('vibrate' in navigator) {
 		try {
-			// Check and request permission if needed
-			// await navigator.permissions.query({ name: 'vibrate' });
-
-			// A short, sharp vibration pattern
-			navigator.vibrate([50]); // Vibrate for 50 milliseconds
+			// A short, sharp vibration pattern, 50ms
+			navigator.vibrate([50]);
 		} catch (error) {
 			console.error('Error triggering vibration:', error);
 		}
